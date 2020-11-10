29ad77dd3559e7393ffcb1c2c21a66158686bd52
Van Step <van.step2186@gmail.com>
2020-11-11 02:45:37 +0400

    add an favicon and some fix

--- static/js/menu.js
+++
@@ -1,8 +0,0 @@
-let menu_is_open = Boolean(document.querySelector('.open-menu'))
-let body = document.querySelector('body')
-let content = document.querySelector('.content')
-
-if(menu_is_open){
-    body.classList.add('hidden-body')
-    content.classList.add('with-shadow')
-}
